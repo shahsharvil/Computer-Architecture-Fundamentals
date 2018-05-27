73915f8a201cd99247a76a5bceccec2f4a1ce387
Shah Sharvil <spshah16@asu.edu>
2018-05-27 14:21:25 -0700

    Add files via upload

---
+++ Read Time Stamp Counter/src.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>		// sleep()
+#include <stdint.h>		// uint32_t, uint64_t
+
+uint64_t read_tsc(void);
+
+
+/*************************************************************************
+Function:    main(void)
+
+Description: TSC read inside a for-loop that calls the read_tsc() function
+			 every second, and prints the value to the console.
+*************************************************************************/
+int main(void)
+{	
+	uint64_t ts = 0;
+
+	for(int i = 1; i < 11; i++)
+	{	
+		ts = read_tsc();
+		printf("Count: %d \t Ticks: %lu\n", i, ts);
+		sleep(1);
+	}
+
+	return 0;
+}
+
+
+/*************************************************************************
+Function:    read_tsc(void)
+
+Called by:   main()
+
+Description: Reads and returns the current TSC value
+
+Ret type:    unsigned 64 bit integer
+*************************************************************************/
+uint64_t read_tsc(void)
+{
+	uint32_t a, d;
+	__asm__ volatile("rdtsc" : "=a" (a), "=d" (d));
+	return (( (uint64_t)a) | ((uint64_t)d) << 32);
+}
