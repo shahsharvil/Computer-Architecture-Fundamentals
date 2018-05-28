a9c946a66f256d9a017dc57b7da575836b81df74
Shah Sharvil <spshah16@asu.edu>
2018-05-28 14:33:26 -0700

    Update src.c

--- Processor-Endianness/src.c
+++ Processor-Endianness/src.c
@@ -5,20 +5,33 @@
 int main()
 {
 
-    int t = 0;
-    int num = 0x99874568;
-    // big end is the byte that contains 0x99
-    // big end refers to the position in the word, not the value of the byte
+    int t = 0, num = 0x99887766;
 
-    //big endian byte order: MSB placed at the byte with lowest address
-
+    // num_byte_addr is an array of unsigned char *, that will be used 
+    // to store the addresses of individual bytes in num
+    unsigned char *num_byte_addr[sizeof(num)];
+ 	
     unsigned char *start = (unsigned char *)&num;
 
+    printf("num: 0x%.2x\n\n", num);
+    
+    // print address and value for each byte of num
     for(t = 0; t < sizeof(num); t++)
-    {
-        printf("Address: %p Value: %.2x\n", start, *start);
+    {	
+    	num_byte_addr[t] = start;
+        printf("Address: %p \t Value: 0x%.2x\n", start, *start);
         start++;
     }
 
+    // make decision about processor endianness
+    if(num_byte_addr[0] < num_byte_addr[1] && *num_byte_addr[0] == 0x66)
+    {
+        printf("\nA Little Endian processor\n");
+    }
+    else
+    {
+        printf("\nA Big Endian processor\n");
+    }
+    
     return 0;
 }
